2f3c8e8f582c38fa69a03827dcd51e9a4304fc5a
wesleyeduardo.dev <wesleyeduardo.dev@gmail.com>
2023-12-12 10:02:16 -0300

    Update ClientAPI.java

--- src/main/java/br/com/thehome/controller/ClientAPI.java
+++ src/main/java/br/com/thehome/controller/ClientAPI.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RequestMapping("/api/v1/clients")
-@CrossOrigin(origins = {"http://localhost:4200", "https://thehome-web.vercel.app", "http://thehome-web.vercel.app"})
+@CrossOrigin(origins = {"http://localhost:4200", "http://thehome-angular-app.s3-website-us-east-1.amazonaws.com", "https://thehome-web.vercel.app", "http://thehome-web.vercel.app"})
 public interface ClientAPI {
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
